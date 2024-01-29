181c586870c617e2b6a2d94e1d19a2fe998e2ae8
MuhammedEmadd <87347836+MuhammedEmadd@users.noreply.github.com>
2024-01-29 17:48:17 +0200

    Add files via upload
    error fix

--- toy.h
+++ toy.h
@@ -19,12 +19,6 @@
 
 #define NEG(X) (TK_Q - (X))
 
-static void toy_fill_small(short *buf, int n);
-static void toy_polmul_naive(short *dst, const short *a, const short *b, int add);
-static void toy_mulmv(short *dst, const short *mat, const short *vec);
-static void toy_mulmTv(short *dst, const short *mat, const short *vec);
-static void toy_dot(short *dst, const short *v1, const short *v2);
-static void toy_add(short *dst, const short *v1, const short *v2, int count, int v2_neg);
 void toy_gen(short *A, short *t, short *s);
 void toy_enc(const short *A, const short *t, int plain, short *u, short *v);
 int toy_dec(const short *s, const short *u, const short *v);
